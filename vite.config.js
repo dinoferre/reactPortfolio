d67c562dcc223abde05a913d92ab230283e53aed
Dino Ferré <dinoferre@hotmail.com>
2024-06-24 15:10:39 -0300

    deploy

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-react-2024/',
+  base: '/reactPortfolio/',
   plugins: [react()],
   build: {
     rollupOptions: {
